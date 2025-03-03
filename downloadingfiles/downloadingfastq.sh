aedfe3dcd404233d6abe238cf3962a8516437d59
Maxwell Konnaris <69332438+maxwellkonnaris@users.noreply.github.com>
2025-03-03 12:19:58 -0500

    Update downloadingfastq.sh

--- downloadingfiles/downloadingfastq.sh
+++ downloadingfiles/downloadingfastq.sh
@@ -10,7 +10,7 @@
 #######################################
 # Configuration
 #######################################
-BATCH_SIZE=75
+BATCH_SIZE=50
 
 WORKDIR="$(pwd)"
 CHECKPOINT_FILE="${WORKDIR}/completed_accessions.txt"
