481ab3c335adf7345899d5cbd4c9d8e6e2e998a1
Maxwell Konnaris <69332438+maxwellkonnaris@users.noreply.github.com>
2025-03-05 18:42:44 -0500

    Update metadatafromfastq.sh

--- metadatafromfastq.sh
+++ metadatafromfastq.sh
@@ -29,7 +29,7 @@ done
 LOCKFILE="/tmp/fastq_processing.lock"
 
 # Ensure output file exists with header row
-echo -e "Filename\tAccession\tFormat\tInstrument\tRun\tFlowcell_ID\tLane\tTile\tX\tY\tRead\tIsFiltered\tControl\tIndex" > "$OUTPUT_FILE"
+echo -e "Filename\tAccession\tRead\tFormat\tInstrument\tRun\tFlowcell_ID\tLane\tTile\tX\tY\tReadLength\tIsFiltered\tControl\tIndex" > "$OUTPUT_FILE"
 
 # Function to process a single FASTQ file
 process_fastq() {
@@ -40,25 +40,32 @@ process_fastq() {
     zcat "$file" | sed -n '1~4p' | awk -v filename="$file" -v accession="$accession" '
     BEGIN { OFS="\t" }
     {
+        # 1. SRA/ENA/DDBJ Format (e.g., @SRR27930322.1 M03047:101:000000000-KMJW5:1:1101:16949:1000 length=246)
         if ($1 ~ /^@(SRR|ERR|DRR)/) {
-            match($0, /^@([^ ]+) ([^:]+):([^:]+):([^:]+):([^:]+):([^:]+):([^:]+):([^ ]+) length=[0-9]+/, a);
+            match($0, /^@([^ ]+)\.([0-9]+) ([^:]+):([^:]+):([^:]+):([^:]+):([^:]+):([^:]+):([^ ]+) length=([0-9]+)/, a);
             if (RLENGTH > 0) {
-                print filename, accession, "SRA/ENA/DDBJ", a[2], a[3], a[4], a[5], a[6], a[7], a[8], "-", "-", "-", "-";
+                print filename, accession, a[2], "SRA/ENA/DDBJ", a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], "-", "-", "-";
             }
+        
+        # 2. Illumina CASAVA 1.8+ Format
         } else if ($1 ~ /^@[A-Za-z0-9]+:[0-9]+:/) {
             match($0, /^@([^:]+):([^:]+):([^:]+):([^:]+):([^:]+):([^:]+):([^ ]+) ([^:]+):([^:]+):([^:]+):([^ ]+)/, a);
             if (RLENGTH > 0) {
-                print filename, accession, "Illumina-CASAVA1.8+", a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11];
+                print filename, accession, "-", "Illumina-CASAVA1.8+", a[1], a[2], a[3], a[4], a[5], a[6], a[7], "-", a[8], a[9], a[10];
             }
+        
+        # 3. Old Illumina Format
         } else if ($1 ~ /^@[A-Za-z0-9-]+:[0-9]+:/) {
             match($0, /^@([^:]+):([^:]+):([^:]+):([^:]+):([^ ]+)#([^ ]+)\/([12])/, a);
             if (RLENGTH > 0) {
-                print filename, accession, "Illumina-Old", a[1], "-", "-", a[2], a[3], a[4], a[5], a[7], "-", "-", a[6];
+                print filename, accession, a[7], "Illumina-Old", a[1], "-", "-", a[2], a[3], a[4], a[5], "-", "-", "-", a[6];
             }
+
+        # 4. Restored SRA Format
         } else if ($1 ~ /^@[0-9]+_[A-Za-z0-9-]+_s_[0-9]+:/) {
             match($0, /^@([^_]+)_([^_]+)_s_([^:]+):([^:]+):([^:]+):([^:]+):([^ ]+)/, a);
             if (RLENGTH > 0) {
-                print filename, accession, "SRA-Restored", "-", "-", "-", a[3], a[4], a[5], a[6], "-", "-", "-", "-";
+                print filename, accession, "-", "SRA-Restored", "-", "-", "-", a[3], a[4], a[5], a[6], "-", "-", "-", "-";
             }
         }
     }' | flock "$LOCKFILE" tee -a "$OUTPUT_FILE" >/dev/null
@@ -70,7 +77,7 @@ export OUTPUT_FILE LOCKFILE
 # Get number of available CPUs from SLURM
 NUM_CPUS=$SLURM_CPUS_PER_TASK
 if [ -z "$NUM_CPUS" ]; then
-    NUM_CPUS=4  # Default to 4 if not running in SLURM
+    NUM_CPUS=4
 fi
 
 echo "Using $NUM_CPUS CPUs for parallel processing"
