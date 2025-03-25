7a3c45b3d3b3f933845a47bddbda2980530b1a54
Maxwell Konnaris <69332438+maxwellkonnaris@users.noreply.github.com>
2025-03-24 23:50:25 -0400

    Update obtainstudyaccessions.sh

--- downloadingfiles/obtainstudyaccessions.sh
+++ downloadingfiles/obtainstudyaccessions.sh
@@ -9,7 +9,7 @@ mkdir -p logs
 # Loop through each study and submit a separate SLURM job
 while read -r study_id; do
     # Submit the job and pass the study ID as a parameter
-    sbatch --job-name=fetch_${study_id} --output=logs/${study_id}.out --error=logs/${study_id}.err fetch_runaccessions.sh "$study_id"
+    sbatch --job-name=fetch_${study_id} --output=logs/${study_id}.out --error=logs/${study_id}.err fetch_runaccession.sh "$study_id"
     echo "✅ Submitted job for $study_id"
 done < "$STUDY_FILE"
 
